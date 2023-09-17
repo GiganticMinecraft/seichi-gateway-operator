cd734c346730282bc6875831ee1adb646f4b56a5
Kory <korygm33@gmail.com>
2023-09-18 03:09:44 +0900

    fix: rbac resource type reference

--- internal/controller/reconcile.go
+++ internal/controller/reconcile.go
@@ -15,12 +15,12 @@ import (
 	"text/template"
 )
 
-//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways/finalizers,verbs=update
-//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigtemplates,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigtemplates/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigtemplates/finalizers,verbs=update
+//+kubebuilder:rbac:groups=seichi.click,resources=seichiassistdebugenvrequest,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=seichi.click,resources=seichiassistdebugenvrequest/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=seichi.click,resources=seichiassistdebugenvrequest/finalizers,verbs=update
+//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigmaptemplates,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigmaptemplates/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=seichi.click,resources=bungeeconfigmaptemplates/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;update;patch
 
 func ReconcileAllManagedResources(ctx context.Context, k8sClient client.Client) (ctrl.Result, error) {
