4666e9b984dacbe387ccae101d8b7abcda77a37b
unchama <11990197+unchama@users.noreply.github.com>
2023-09-17 13:24:45 +0900

    refactor: remove unnecessary files and rename group

--- api/v1alpha1/seichireviewgateway_types.go
+++ api/v1alpha1/seichireviewgateway_types.go
@@ -1,42 +1,15 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // SeichiReviewGatewaySpec defines the desired state of SeichiReviewGateway
 type SeichiReviewGatewaySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// 稼働環境を生成する時にどのプルリクエスト番号時点のリポジトリデータを参照するかの設定
+	// SeichiAssist リポジトリの Pull Request で Ready for review になっていて
+	// デバッグ環境を必要としているものの番号
 	PullRequestNo int `json:"pullRequestNo"`
-}
-
-// SeichiReviewGatewayStatus defines the observed state of SeichiReviewGateway
-type SeichiReviewGatewayStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 }
 
 //+kubebuilder:object:root=true
@@ -47,8 +20,7 @@ type SeichiReviewGateway struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   SeichiReviewGatewaySpec   `json:"spec,omitempty"`
-	Status SeichiReviewGatewayStatus `json:"status,omitempty"`
+	Spec SeichiReviewGatewaySpec `json:"spec,omitempty"`
 }
 
 //+kubebuilder:object:root=true
--- internal/controller/seichireviewgateway_controller.go
+++ internal/controller/seichireviewgateway_controller.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
@@ -33,9 +17,9 @@ type SeichiReviewGatewayReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=seichiclick.seichi.click,resources=seichireviewgateways,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=seichiclick.seichi.click,resources=seichireviewgateways/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=seichiclick.seichi.click,resources=seichireviewgateways/finalizers,verbs=update
+//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=seichi.click,resources=seichireviewgateways/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
